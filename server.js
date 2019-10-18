6df278e4f7542c2e9c21d0fe4f4b61b06a5cc32b
Magus <mfail792@gmail.com>
2019-10-18 17:39:58 -0500

    created server file, installed packages

---
+++ server.js
@@ -0,0 +1,28 @@
+//Starter point for node/express server
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//Setting up express app
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+//Setting up the express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//Static directory
+app.use(express.static("app/public"));
+
+//Routing
+require("./app/routes/api-routes.js")(app);
+require("./app/routes/html-routes.js")(app);
+
+//Starts server to begin listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
