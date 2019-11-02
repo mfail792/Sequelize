a3c3cfa59cf8f139cd176456413f36d3d5c944ff
Magus <mfail792@gmail.com>
2019-11-01 23:04:08 -0500

    added on click event for adding book within add.js, added html routes for page loads

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -6,30 +6,30 @@ module.exports = function (app) {
 
     //index route that loads index.html
     app.get("/", function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/view.html"));
+        res.sendFile(path.join(__dirname, "../public/view.html"));
     });
 
     //add route loading the add.html page
     app.get("/add", function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/add.html"));
+        res.sendFile(path.join(__dirname, "../public/add.html"));
     });
 
     //route for loading all.html page
     //where all books in DB are displayed
     app.get("/all", function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/all.html"));
+        res.sendFile(path.join(__dirname, "../public/all.html"));
     });
 
     //route for loading short.html page
     //where short books in the DB are displayed
     app.get("/short", function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/short.html"));
+        res.sendFile(path.join(__dirname, "../public/short.html"));
     });
 
     //long route loading the long.html page
     //where long books in the DB are displayed
     app.get("/long", function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/long.html"));
+        res.sendFile(path.join(__dirname, "../public/long.html"));
     });
 
 };
