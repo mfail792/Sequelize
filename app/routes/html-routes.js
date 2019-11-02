924821155a61e872d9c8529a8d2a40307f0429b7
Magus <mfail792@gmail.com>
2019-11-01 22:27:56 -0500

    added html routes for page loads

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,6 +1,35 @@
 var path = require("path");
 
-module.exports = function(app) {
 
+
+module.exports = function (app) {
+
+    //index route that loads index.html
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(_dirname, "../public/view.html"));
+    });
+
+    //add route loading the add.html page
+    app.get("/add", function (req, res) {
+        res.sendFile(path.join(_dirname, "../public/add.html"));
+    });
+
+    //route for loading all.html page
+    //where all books in DB are displayed
+    app.get("/all", function (req, res) {
+        res.sendFile(path.join(_dirname, "../public/all.html"));
+    });
+
+    //route for loading short.html page
+    //where short books in the DB are displayed
+    app.get("/short", function (req, res) {
+        res.sendFile(path.join(_dirname, "../public/short.html"));
+    });
+
+    //long route loading the long.html page
+    //where long books in the DB are displayed
+    app.get("/long", function (req, res) {
+        res.sendFile(path.join(_dirname, "../public/long.html"));
+    });
 
 };
